07775f5275f14b0018deca32256fbb8a66f40ae8
baksakcci <pshsh910@gmail.com>
2024-07-23 23:25:19 +0900

    project init

---
+++ src/main/java/myongari/backend/MyongariBackendApplication.java
@@ -0,0 +1,13 @@
+package myongari.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyongariBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyongariBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/myongari/backend/MyongariBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package myongari.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyongariBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
