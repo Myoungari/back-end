d05ca5269e586631accaf25b4843613159dc7f68
sangcci <pshsh910@gmail.com>
2024-08-12 21:55:46 +0900

    refactor: page, size 대신 `Pageable` 객체로 받아오도록 수정

--- src/main/java/myongari/backend/club/presentation/ClubController.java
+++ src/main/java/myongari/backend/club/presentation/ClubController.java
@@ -6,11 +6,12 @@ import myongari.backend.club.domain.Club;
 import myongari.backend.club.presentation.dto.ClubName;
 import myongari.backend.club.presentation.dto.ClubSimplePage;
 import myongari.backend.common.response.Success;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -23,8 +24,8 @@ public class ClubController {
     private final ClubService clubService;
 
     @GetMapping
-    public ResponseEntity<Success> findAllSimpleInfo(@RequestParam int page, @RequestParam int size) {
-        ClubSimplePage clubSimpleAll = clubService.getClubSimpleAll(page, size);
+    public ResponseEntity<Success> findAllSimpleInfo(@PageableDefault Pageable pageable) {
+        ClubSimplePage clubSimpleAll = clubService.getClubSimpleAll(pageable);
         return ResponseEntity.status(200)
                 .body(Success.of(200, clubSimpleAll));
     }
