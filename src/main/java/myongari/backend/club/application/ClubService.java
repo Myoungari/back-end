cd471c760e5c240a66e618505f155ed45bf3a88c
sangcci <pshsh910@gmail.com>
2024-08-12 10:05:59 +0900

    feat: 트랜잭션 추가

--- src/main/java/myongari/backend/club/application/ClubService.java
+++ src/main/java/myongari/backend/club/application/ClubService.java
@@ -9,6 +9,7 @@ import myongari.backend.club.presentation.dto.ClubSimplePage;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -18,6 +19,7 @@ public class ClubService {
 
     private final ClubRepository clubRepository;
 
+    @Transactional(readOnly = true)
     public ClubSimplePage getClubSimpleAll(int page, int size) {
         PageRequest pageRequest = PageRequest.of(page, size);
 
@@ -26,10 +28,12 @@ public class ClubService {
         return ClubSimplePage.from(clubSimpleAll);
     }
 
+    @Transactional(readOnly = true)
     public List<ClubName> getClubNamesByCategory(String categoryName) {
         return clubRepository.getClubNamesByCategory(categoryName);
     }
 
+    @Transactional(readOnly = true)
     public ClubCount getClubCount() {
         return clubRepository.getClubCount();
     }
