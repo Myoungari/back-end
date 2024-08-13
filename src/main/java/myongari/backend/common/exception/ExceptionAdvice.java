8aa5d41fb7bd85a4fb08be9e5dd5d0591905de3e
sangcci <pshsh910@gmail.com>
2024-08-13 12:21:21 +0900

    feat: 유효성 검사 및 예외 응답 처리 추가
    - 잘못된 url 요청에 대한 `NoResourceFoundException` 처리 추가
    - 이외에 모든 예외는 500으로 처리
    - 예외 처리 메서드 네이밍에 `handle` 접두어 추가
    - `ResponseEntityExceptionHandler` 상속
    - logging 추가

--- src/main/java/myongari/backend/common/exception/ExceptionAdvice.java
+++ src/main/java/myongari/backend/common/exception/ExceptionAdvice.java
@@ -1,23 +1,47 @@
 package myongari.backend.common.exception;
 
+import lombok.extern.slf4j.Slf4j;
+import myongari.backend.common.response.Error;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import java.util.NoSuchElementException;
 
 @RestControllerAdvice
-public class ExceptionAdvice {
+@Slf4j
+public class ExceptionAdvice extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(NoSuchElementException.class)
-    public ResponseEntity<Error> noSuchElementException(NoSuchElementException e) {
+    public ResponseEntity<Error> handleNoSuchElementException(NoSuchElementException e) {
+        log.warn("handleNoSuchElementException", e);
         return ResponseEntity.status(404)
-                .body(new Error(e.getMessage()));
+                .body(Error.of(404, e.getMessage()));
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<Error> illegalArgumentException(IllegalArgumentException e) {
+    public ResponseEntity<Error> handleIllegalArgumentException(IllegalArgumentException e) {
+        log.warn("handleIllegalArgumentException", e);
         return ResponseEntity.status(400)
-                .body(new Error(e.getMessage()));
+                .body(Error.of(400, e.getMessage()));
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleNoResourceFoundException(NoResourceFoundException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        log.warn("handleNoResourceFoundException", ex);
+        return ResponseEntity.status(400)
+                .body(Error.of(400, "잘못된 요청입니다."));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Error> handleAllException(Exception e) {
+        log.warn("handleAllException", e);
+        return ResponseEntity.status(500)
+                .body(Error.of(500, "서버 내부에 오류가 발생했습니다."));
     }
 }
