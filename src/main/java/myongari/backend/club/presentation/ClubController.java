450874c095f7e959b73b8c2e70537a54acd9d5c7
sangcci <pshsh910@gmail.com>
2024-08-13 11:40:49 +0900

    fix: rest api 명명 규칙에 맞게 url 수정

--- src/main/java/myongari/backend/club/presentation/ClubController.java
+++ src/main/java/myongari/backend/club/presentation/ClubController.java
@@ -11,33 +11,31 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/clubs")
 public class ClubController {
 
     private final ClubService clubService;
 
-    @GetMapping
+    @GetMapping("/clubs")
     public ResponseEntity<Success> findAllSimpleInfo(@PageableDefault Pageable pageable) {
         ClubSimplePage clubSimpleAll = clubService.getClubSimpleAll(pageable);
         return ResponseEntity.status(200)
                 .body(Success.of(200, clubSimpleAll));
     }
 
-    @GetMapping("/{category_name}")
+    @GetMapping("/categories/{category_name}/clubs")
     public ResponseEntity<Success> getClubNamesByCategory(@PathVariable(name = "category_name") String categoryName) {
         List<ClubName> clubNamesByCategory = clubService.getClubNamesByCategory(categoryName);
         return ResponseEntity.status(200)
                 .body(Success.of(200, clubNamesByCategory));
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/clubs/{id}")
     public ResponseEntity<Success> getClubById(@PathVariable(name = "id") int id) {
         Club club = clubService.getClubById(id);
         return ResponseEntity.status(200)
