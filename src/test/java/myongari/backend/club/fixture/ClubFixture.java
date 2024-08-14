c12f89abfc1405a6e226dc22ce0469fadcbb902a
sangcci <pshsh910@gmail.com>
2024-08-14 16:53:19 +0900

    test: 테스트 Fixture 구조 리팩토링

--- src/test/java/myongari/backend/club/fixture/ClubFixture.java
+++ src/test/java/myongari/backend/club/fixture/ClubFixture.java
@@ -11,19 +11,6 @@ import java.util.List;
 @SuppressWarnings("NonAsciiCharacters")
 public class ClubFixture {
 
-    public static List<Club> 모든_동아리_정보_생성() {
-        President 회장_1_정보 = new President("홍길동", "010-1111-1111", "example1@example.com");
-        President 회장_2_정보 = new President("김철수", "010-2222-2222", "example2@example.com");
-
-        Apply 동아리_1_지원_정보 = new Apply(State.Recruited, "https://form.example.com/1", "지원 조건 예시1");
-        Apply 동아리_2_지원_정보 = new Apply(State.Recruiting, "https://form.example.com/2", "지원 조건 예시2");
-
-        return List.of(new Club("동아리1", "학관1111", "https://sns.example.com/1", "https://image.example.com/1", "소개글 예시1", "활동 예시1",
-                        동아리_1_지원_정보, 회장_1_정보, 카테고리1_생성()),
-                new Club("동아리2", "학관2222", "https://sns.example.com/2", "https://image.example.com/2", "소개글 예시2", "활동 예시2",
-                        동아리_2_지원_정보, 회장_2_정보, 카테고리1_생성()));
-    }
-
     public static List<Club> 모든_동아리_정보_생성(Club ...club) {
         return List.of(club);
     }
