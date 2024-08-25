3873dd3afc5bec46f62ce52ae9d9a4c12c66922f
sangcci <pshsh910@gmail.com>
2024-08-26 02:17:35 +0900

    hotfix: contextLoads() 에러 해결2
    - contextLoads() 제거

--- src/test/java/myongari/backend/MyongariBackendApplicationTests.java
+++ src/test/java/myongari/backend/MyongariBackendApplicationTests.java
@@ -1,13 +1,5 @@
 package myongari.backend;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest(classes = MyongariBackendApplication.class)
 class MyongariBackendApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
