03d64c4a4990eb1918bd185fef444d9d88448a8c
sangcci <pshsh910@gmail.com>
2024-08-26 01:54:40 +0900

    hotfix: contextLoads() 에러 해결

--- src/test/java/myongari/backend/MyongariBackendApplicationTests.java
+++ src/test/java/myongari/backend/MyongariBackendApplicationTests.java
@@ -3,7 +3,7 @@ package myongari.backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = MyongariBackendApplication.class)
 class MyongariBackendApplicationTests {
 
 	@Test
