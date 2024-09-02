6a2a8a91a54ae4f93a88aa80a7f9fbf9b2b5d729
sangcci <pshsh910@gmail.com>
2024-09-02 22:12:54 +0900

    feat: 카테고리별 동아리 조회 기능 추가
    - id값이 없다면 default로 모집 중 상태의 가나다 순 제일 첫번째 동아리 상세 정보 출력

--- src/main/java/myongari/backend/club/application/ClubService.java
+++ src/main/java/myongari/backend/club/application/ClubService.java
@@ -10,6 +10,7 @@ import myongari.backend.club.application.port.ClubRepository;
 import myongari.backend.club.domain.Club;
 import myongari.backend.club.domain.Image;
 import myongari.backend.club.presentation.dto.ClubName;
+import myongari.backend.club.presentation.dto.ClubNamesAndDetail;
 import myongari.backend.club.presentation.dto.ClubSimple;
 import myongari.backend.club.presentation.dto.ClubSimplePage;
 import org.springframework.data.domain.Page;
@@ -43,11 +44,58 @@ public class ClubService {
     }
 
     @Transactional(readOnly = true)
-    public List<ClubName> findClubNamesByCategoryName(String categoryName) {
+    public ClubNamesAndDetail findClubNamesAndClubDetailByCategoryName(String categoryName) {
         categoryRepository.findCategoryByName(categoryName)
                 .orElseThrow(() -> new NoSuchElementException("카테고리를 찾지 못했습니다."));
 
-        return clubRepository.findClubNamesByCategoryName(categoryName);
+        List<ClubName> clubNames = clubRepository.findClubNamesByCategoryName(categoryName);
+        ClubName clubNameFirst = clubNames.stream()
+                .findFirst()
+                .orElseThrow(() -> new NoSuchElementException("카테고리 내 동아리가 존재하지 않습니다."));
+
+        Club clubDetail = clubRepository.findClubById(clubNameFirst.getId())
+                .orElseThrow((() -> new NoSuchElementException("동아리를 찾지 못했습니다.")));
+
+        Image image = clubDetail.getImage();
+        if (image != null) {
+            Image downloaded = clubImageStorage.downloadImage(image.getUuid());
+            clubDetail.setImage(downloaded);
+        }
+
+        return ClubNamesAndDetail.builder()
+                .clubNames(clubNames)
+                .club(clubDetail)
+                .build();
+    }
+
+    @Transactional(readOnly = true)
+    public ClubNamesAndDetail findClubNamesAndClubDetailByCategoryName(String categoryName, long id) {
+        categoryRepository.findCategoryByName(categoryName)
+                .orElseThrow(() -> new NoSuchElementException("카테고리를 찾지 못했습니다."));
+
+        List<ClubName> clubNames = clubRepository.findClubNamesByCategoryName(categoryName);
+        if (clubNames.isEmpty()) {
+            throw new NoSuchElementException("카테고리 내 동아리가 존재하지 않습니다.");
+        }
+
+        Club clubAsCategory = clubNames.stream()
+                .map(ClubName::getId)
+                .filter(clubId -> clubId == id)
+                .map(clubId -> clubRepository.findClubById(clubId)
+                        .orElseThrow(() -> new NoSuchElementException("동아리를 찾지 못했습니다.")))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("동아리가 카테고리 내에 속해있지 않습니다."));
+
+        Image image = clubAsCategory.getImage();
+        if (image != null) {
+            Image downloaded = clubImageStorage.downloadImage(image.getUuid());
+            clubAsCategory.setImage(downloaded);
+        }
+
+        return ClubNamesAndDetail.builder()
+                .clubNames(clubNames)
+                .club(clubAsCategory)
+                .build();
     }
 
     @Transactional(readOnly = true)
--- src/main/java/myongari/backend/club/presentation/dto/ClubNamesAndDetail.java
+++ src/main/java/myongari/backend/club/presentation/dto/ClubNamesAndDetail.java
@@ -2,12 +2,14 @@ package myongari.backend.club.presentation.dto;
 
 import java.util.List;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import myongari.backend.club.domain.Club;
 
 @Getter
 @AllArgsConstructor
-public class ClubCategory {
+@Builder
+public class ClubNamesAndDetail {
 
     private List<ClubName> clubNames;
     private Club club;
