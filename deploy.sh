6f4dcce6bb198da84f4679ed5951eefa40fe9c4c
sangcci <pshsh910@gmail.com>
2025-02-18 02:36:11 +0900

    deploy: sqlite db file github action secret 배포

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+cd /home/ubuntu
+
+if [ -f "database.db" ]; then
+  echo "📂 Backing up the existing SQLite database..."
+  mv database.db database.db.bak
+  rm -f database.db
+fi
+
+echo "🚀 0. Applying the new SQLite database..."
+mv database.new.db database.db
 
 echo "1. Checking if the springboot container is running..."
 if [ "$(docker ps -q -f name=springboot)" ]; then
